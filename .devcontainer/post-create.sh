5616bfba9b7772d3f43a73a13e0c027945bc9b06
ZhuZhen <71745781+ZhuZhen200@users.noreply.github.com>
2023-09-29 10:16:42 +0000

    Added .devcontainer configuration for automatic setup in Codespaces

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -6,8 +6,15 @@ apt-get update && apt-get upgrade -y
 # Install C++ Toolchain
 apt-get install -y build-essential
 
+# Install required system dependencies (if any)
+# apt-get install -y <other-dependencies>
+
 # Install R Packages
 Rscript -e "install.packages(c('coda','mvtnorm','devtools','loo','dagitty','shape'), repos='http://cran.rstudio.com/')"
+
+# Install cmdstanr and compile libraries
 Rscript -e "devtools::install_github('stan-dev/cmdstanr', dependencies=c('Depends', 'Imports'))"
 Rscript -e "cmdstanr::install_cmdstan()"
+
+# Install rethinking package
 Rscript -e "devtools::install_github('rmcelreath/rethinking')"
